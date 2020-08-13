213a809cf35acf2dafb97147c836c093916027bf
VKBora <virendrakbora@gmail.com>
2020-08-13 23:54:36 +0530

    All Files Commit

---
+++ ThreadsDemo/src/threadsPack/Example1.java
@@ -0,0 +1,34 @@
+package threadsPack;
+
+class A implements Runnable
+{
+	public void run() 
+	{
+		
+	for(int i = 0; i<=10; i++)
+		System.out.println("Thread A " + i);	
+	}
+}
+
+class B implements Runnable
+{
+	public void run() 
+	{
+		
+	for(int i = 0; i<=10; i++)
+		System.out.println("Thread B " + i);	
+	}
+}
+
+public class Example1 {
+
+public static void main (String[] args)
+{
+	Thread t1 = new Thread(new A());
+	Thread t2 = new Thread(new B());
+	t1.start();
+	t2.start();
+}
+
+}
+
